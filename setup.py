4698d6bc1838eb906beadf6c920c7391412fb57d
wangyibin <738339463@qq.com>
2022-08-06 10:13:25 +0800

    init setup

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+import os.path as op
+from setuptools import setup, find_packages
+from setup_helper import SetupHelper
+
+classifiers = [
+    "Development Status :: 2 - Pre-Alpha",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: BSD License",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Topic :: Scientific/Engineering :: Bio-Informatics"
+]
+
+NAME = "ALLHiC2"
+h = SetupHelper(initfile="allhic2/__init__.py", readmefile="README.md")
+setup_dir = op.abspath(op.dirname(__file__))
+requirements = [x.strip() for x in open(op.join(setup_dir, "requirements.txt"))]
+
+
+
+setup(
+    name=NAME,
+    author=h.author,
+    author_email=h.email,
+    version=h.version,
+    license=h.license,
+    classifiers=classifiers,
+    packages=find_packages(),
+    zip_safe=False,
+    url='http://github.com/wangyibin/ALLHiC2',
+    description="Genome scaffold by Hi-C",
+    long_description=h.long_description,
+    long_description_content_type='test/markdown',
+    install_requires=requirements
+)
